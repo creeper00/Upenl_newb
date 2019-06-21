73358997251de4dd513d70695f49341a53ec3b21
creeper00 <creeper000@naver.com>
2019-06-21 22:33:26 +0900

    Updates

---
+++ precache-manifest.4fdff2c8518519b031976b2bf99dab65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a893fc7406d9c658de29cc2d1b1410",
+    "url": "/Upnel_newb/index.html"
+  },
+  {
+    "revision": "5f41a3d1fdcdcbd10246",
+    "url": "/Upnel_newb/static/css/main.b80ac5d4.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/Upnel_newb/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "5f41a3d1fdcdcbd10246",
+    "url": "/Upnel_newb/static/js/main.d3bf6ee0.chunk.js"
+  },
+  {
+    "revision": "31538487b5976409a2ee",
+    "url": "/Upnel_newb/static/js/runtime~main.df4a6d87.js"
+  }
+]);
